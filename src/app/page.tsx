4a5b144ebf0e1a2a19969177474384af7d1b93bf
Gabrielk127 <gabrielfsilva09@gmail.com>
2025-02-24 13:28:55 -0300

    add new hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,21 @@
 
 import Header from "@/components/Header/Header";
 import Testimonials from "@/components/Sections/Testimonials";
-import Hero from "@/components/Sections/Hero";
+// import Hero from "@/components/Sections/Hero";
 import MyWork from "@/components/Sections/MyWork";
 import ProfessionalExperience from "@/components/Sections/ProfessionalExperience";
 import ProjectGrid from "@/components/Sections/ProjectCard";
 import Freelance from "@/components/Sections/Freelance";
 import Footer from "@/components/Header/Footer";
+import RainingLetters from "@/components/Sections/HeroRaining";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <section id="inicio">
-        <Hero />
+        <RainingLetters />
+        {/* <Hero /> */}
       </section>
       <section>
         <MyWork />

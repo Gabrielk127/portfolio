d0a5790c8f2077bba573b057e15edb1df94d0d3d
Gabrielk127 <gabrielfsilva09@gmail.com>
2025-03-24 08:57:55 -0300

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -20,6 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <Analytics />
       <body className={`bg-[#1E1E1E] ${poppins}`}>{children}</body>
     </html>
   );
